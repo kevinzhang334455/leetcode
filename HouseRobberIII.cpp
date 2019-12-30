30a9c872d4408b28f7e8f868793f5988a9caba59
kevinzhang334455 <kevinzhang334455@gmail.com>
2019-12-29 20:52:41 -0800

    format

--- HouseRobberIII.cpp
+++ HouseRobberIII.cpp
@@ -10,11 +10,10 @@ using std::queue;
 
 struct TreeNode
 {
-	int val;
-	TreeNode* left;
-	TreeNode* right;
-
-	TreeNode(int x): val(x), left(nullptr), right(nullptr) {}
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+  TreeNode(int x): val(x), left(nullptr), right(nullptr) {}
 };
 
 // We can use BFS
--- WiggleSequence.cpp
+++ WiggleSequence.cpp
@@ -11,32 +11,31 @@ using namespace std;
 
 class Solution {
 public:
-    int wiggleMaxLength(vector<int>& nums) {
-        const int N = nums.size();
-        vector<int> dp(2 * N, 1);
-        
-        for (int i = 1; i < N; i++)
+  int wiggleMaxLength(vector<int>& nums) {
+    const int N = nums.size();
+    vector<int> dp(2 * N, 1);
+    for (int i = 1; i < N; i++)
+    {
+      for (int j = 0; j < i; j++)
+      {
+        if (nums[i] - nums[j] > 0)
         {
-            for (int j = 0; j < i; j++)
-            {
-                if (nums[i] - nums[j] > 0)
-                {
-                    dp[2 * i] = max(dp[2 * i], dp[2 * j + 1] + 1);
-                }
-                else if (nums[i] - nums[j] < 0)
-                {
-                    dp[2 * i + 1] = max(dp[2 * i + 1], dp[2 * j] + 1);
-                }
-            }
+          dp[2 * i] = max(dp[2 * i], dp[2 * j + 1] + 1);
         }
-        
-        return *max_element(dp.begin(), dp.end());
+        else if (nums[i] - nums[j] < 0)
+        {
+          dp[2 * i + 1] = max(dp[2 * i + 1], dp[2 * j] + 1);
+        }
+      }
     }
+
+    return *max_element(dp.begin(), dp.end());
+  }
 };
 
 int main()
 {
-    Solution S;
-    vector<int> test1 = { 1,17,5,10,13,15,10,5,16,8 };
-    cout << S.wiggleMaxLength(test1) << endl;
+  Solution S;
+  vector<int> test1 = { 1,17,5,10,13,15,10,5,16,8 };
+  cout << S.wiggleMaxLength(test1) << endl;
 }
